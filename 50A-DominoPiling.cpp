9715a7f1782570649465524c337798e1d10a4f8a
Yabets Maregn <yabetsanon@gmail.com>
2025-01-19 12:00:00 +0900

    Add descriptive comment to 50A-DominoPiling.cpp

--- 50A-DominoPiling.cpp
+++ 50A-DominoPiling.cpp
@@ -4,6 +4,9 @@
 using namespace std;
 using namespace tsd;
 
+// Calculates the maximum number of dominoes that can fit on an M x N board.
+// Each domino covers two squares, so the answer is simply (M * N) / 2.
+
 int main(){
     int M = 0, N = 0; scanf("%d %d\n",&M, &N);
     printf("%d\n", M * N / 2);
