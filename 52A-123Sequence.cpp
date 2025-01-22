742541041fb70830ca409a7f27471eaca3ec9e06
Yabets Maregn <yabetsanon@gmail.com>
2025-01-22 12:00:00 +0900

    Add descriptive comment to 52A-123Sequence.cpp

--- 52A-123Sequence.cpp
+++ 52A-123Sequence.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Reads a sequence of numbers and counts the occurrences of each value (1-3).
+// Outputs the sum of the two middle counts after sorting, representing a specific problem's requirement.
 
 int main(){
 
@@ -8,7 +10,7 @@ int main(){
 
     long n; scanf("%ld", &n);
     for(long p = 0; p < n; p++){int temp; scanf("%d", &temp); ++count[temp];}
-    std::sort(count, count + 4);
+    sort(count, count + 4);
     printf("%ld\n", count[1] + count[2]);
 
     return 0;
