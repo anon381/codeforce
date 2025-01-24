ba956ed12bd657e4ff2183f496e6fdada071ccad
Yabets Maregn <yabetsanon@gmail.com>
2025-01-24 12:00:00 +0900

    Add descriptive comment to 54A-Presents.cpp

--- 54A-Presents.cpp
+++ 54A-Presents.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Calculates the minimum number of intervals needed to cover all presents, given their positions and a fixed interval length.
+// Uses arithmetic to determine the number of intervals between presents.
+
 int main(){
 
     long n, k; scanf("%ld %ld", &n, &k);
