47d1a9e1fab6d3931a59c09ae58c97c591f3b1ad
Yabets Maregn <yabetsanon@gmail.com>
2025-01-28 12:00:00 +0900

    Add descriptive comment to 55C-PieOrDie.cpp

--- 55C-PieOrDie.cpp
+++ 55C-PieOrDie.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+// Checks if any of the given k pies are close to the border of an n x m grid.
+// If any pie is within 5 units of the border, prints 'YES'; otherwise, prints 'NO'.
 
+int main(){
     int n, m, k; scanf("%d %d %d\n", &n, &m, &k);
     for(int p = 0; p < k; p++){
         int x, y; scanf("%d %d\n", &x, &y);
