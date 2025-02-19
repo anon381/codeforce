0ff13c31d1e9dd2c852c47a7858ecd907804245d
Yabets Maregn <yabetsanon@gmail.com>
2025-02-19 12:00:00 +0900

    Add loop/condition comments to 79B-ColorfulField.cpp

--- 79B-ColorfulField.cpp
+++ 79B-ColorfulField.cpp
@@ -9,6 +9,7 @@ int main(){
     vector<long> wasteCells(k, 0);
 
     for(long p = 0; p < k; p++){
+        // Store the linear index of each waste cell
         long row, col; scanf("%ld %ld\n", &row, &col);
         wasteCells[p] = m * (row - 1) + (col - 1);
     }
@@ -16,9 +17,11 @@ int main(){
     sort(wasteCells.begin(), wasteCells.end());
 
     for(long p = 0; p < t; p++){
+        // For each query, calculate the cell number
         long row, col; scanf("%ld %ld\n", &row, &col);
         long cellNumber = m * (row - 1) + (col - 1);
         long index = 0, wasteNumber = 0;
+        // Find how many waste cells are before the current cell
         while(index < k){
             if(wasteCells[index] < cellNumber){++index;}
             else if(wasteCells[index] >= cellNumber){break;}
@@ -26,6 +29,7 @@ int main(){
 
         if(wasteCells[index] == cellNumber){puts("Waste");}
         else{
+            // Determine crop type based on position
             long cropNumber = cellNumber - index;
             if(cropNumber % 3 == 0){puts("Carrots");}
             else if(cropNumber % 3 == 1){puts("Kiwis");}
