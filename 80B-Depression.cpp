c260ea4d791a2ed86851cf8e37c5561a11e8a43e
Yabets Maregn <yabetsanon@gmail.com>
2025-02-21 12:00:00 +0900

    Add calculation comments to 80B-Depression.cpp

--- 80B-Depression.cpp
+++ 80B-Depression.cpp
@@ -6,7 +6,9 @@ using namespace std;
 int main(){
 
     int hour(0), minute(0); scanf("%d:%d", &hour, &minute);
+    // Calculate the angle of the hour hand
     double first  = 30 * (hour%12) + minute/2.0;
+    // Calculate the angle of the minute hand
     double second = 6 * minute; 
 
     printf("%.10lf %.10lf", first, second);
