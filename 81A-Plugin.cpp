221003956f5e70e141322332545460d8677ad5fb
Yabets Maregn <yabetsanon@gmail.com>
2025-02-22 12:00:00 +0900

    Add time/space complexity comments to 81A-Plugin.cpp

--- 81A-Plugin.cpp
+++ 81A-Plugin.cpp
@@ -10,6 +10,7 @@ int main(){
 
     for(int k = 0; k < input.size(); k++){
     // If output is empty or last char is not current, add it; otherwise, remove last char
+    // Time: O(n), Space: O(n) - processes each character once, output can be up to n
         if(output.empty() || output.back() != input[k]){output += input[k];}
         else{output.pop_back();}
     }
