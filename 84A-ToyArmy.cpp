411bbd9c46fd1d33094d45f5a619274e591f2cbb
Yabets Maregn <yabetsanon@gmail.com>
2025-03-13 12:00:00 +0900

    Add descriptive function comment to 84A-ToyArmy.cpp

--- 84A-ToyArmy.cpp
+++ 84A-ToyArmy.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
+// Computes the total number of toy soldiers in the army based on input n.
 
 int main(){int n; scanf("%d", &n);printf("%d\n", 3 * n / 2);return 0;}
