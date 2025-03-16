d2eed21f5ebaefe2663cfa93ed954bb7b2c06e43
Yabets Maregn <yabetsanon@gmail.com>
2025-03-16 12:00:00 +0900

    Add step-by-step comments to 88A-Chord.cpp

--- 88A-Chord.cpp
+++ 88A-Chord.cpp
@@ -6,9 +6,10 @@
 using namespace std;
 
 int main(){
-
+    // Number of notes in an octave
     const int M = 12;
 
+    // Map each note to its corresponding integer value
     map<string, int> notes;
     notes["C"] = 0; notes["C#"] = 1;
     notes["D"] = 2; notes["D#"] = 3;
@@ -17,15 +18,32 @@ int main(){
     notes["G"] = 7; notes["G#"] = 8;
     notes["A"] = 9; notes["B"] = 10; notes["H"] = 11;
 
+    // Read the three notes from input
     string x, y, z; cin >> x >> y >> z;
     vector<int> s; 
+    // Convert notes to their integer values and store in vector
     s.push_back(notes[x]); s.push_back(notes[y]); s.push_back(notes[z]);
+    // Sort the notes to simplify interval calculation
     sort(s.begin(), s.end()); 
+    // Add the first note + 12 to handle octave wrap-around
     s.push_back(s[0] + M);
 
-    if((s[1] - s[0] == 4 && s[2] - s[1] == 3) || (s[2] - s[1] == 4 && s[3] - s[2] == 3) || (s[3] - s[2] == 4 && s[1] - s[0] == 3)){puts("major");}
-    else if((s[1] - s[0] == 3 && s[2] - s[1] == 4) || (s[2] - s[1] == 3 && s[3] - s[2] == 4) || (s[3] - s[2] == 3 && s[1] - s[0] == 4)){puts("minor");}
-    else{puts("strange");}
+    // Check for major chord intervals
+    if((s[1] - s[0] == 4 && s[2] - s[1] == 3) || 
+       (s[2] - s[1] == 4 && s[3] - s[2] == 3) || 
+       (s[3] - s[2] == 4 && s[1] - s[0] == 3)){
+        puts("major");
+    }
+    // Check for minor chord intervals
+    else if((s[1] - s[0] == 3 && s[2] - s[1] == 4) || 
+            (s[2] - s[1] == 3 && s[3] - s[2] == 4) || 
+            (s[3] - s[2] == 3 && s[1] - s[0] == 4)){
+        puts("minor");
+    }
+    // If neither, output "strange"
+    else{
+        puts("strange");
+    }
 
     return 0;
 }
