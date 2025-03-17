ee6467f96ef3525e4d9b128d1cf919eaf364dcc4
Yabets Maregn <yabetsanon@gmail.com>
2025-03-17 12:00:00 +0900

    Add step-by-step comments to 89A-Robbery.cpp

--- 89A-Robbery.cpp
+++ 89A-Robbery.cpp
@@ -7,16 +7,24 @@ using namespace std;
 typedef long long ll;
 
 int main(){
-
+    // Read n (number of boxes), m (number of coins per box), k (number of robbers)
     ll n, m, k; scanf("%lld %lld %lld", &n, &m, &k);
-    if(n % 2 == 0){puts("0");}
+    // If n is even, output 0 (no coins needed)
+    if(n % 2 == 0){
+        puts("0");
+    }
     else{
+        // Calculate minimum coins needed for odd n
         ll mn(k * (m  / ((n + 1) / 2)));
+        // Loop through each box
         for (ll p = 0; p < n; p++){
             ll x; scanf("%lld", &x);
-            if(p % 2 == 0){mn = (mn < x) ? mn : x;}
+            // If box index is even, update minimum coins
+            if(p % 2 == 0){
+                mn = (mn < x) ? mn : x;
+            }
         }
-
+        // Output the result
         printf("%lld\n", mn);
     }
 
