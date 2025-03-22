fd2117c96a0d7248b9b63de528acffa420541ff5
Yabets Maregn <yabetsanon@gmail.com>
2025-03-22 12:00:00 +0900

    Update 91B-Queue.cpp

--- 91B-Queue.cpp
+++ 91B-Queue.cpp
@@ -1,16 +1,16 @@
 #include <cstdio>
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main(){
 
     long n; scanf("%ld", &n);
-    std::vector<std::pair<long, long> > a(n); 
+    vector<pair<long, long>> a(n); 
     for(long p = 0; p < n; p++){scanf("%ld", &a[p].first); a[p].second = p;}
     sort(a.begin(), a.end());
     long mx(-1);
-    std::vector<long> f(n); 
+    vector<long> f(n); 
     for(long p = 0; p < n; p++){
         f[a[p].second] = (a[p].second > mx) ? (-1) : (mx - a[p].second - 1);
         mx = (mx > a[p].second) ? mx : a[p].second;
