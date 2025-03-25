78c5244ceecfdd8314da62e5b07115083f8c7703
Yabets Maregn <yabetsanon@gmail.com>
2025-03-25 12:00:00 +0900

    Update 92B-BinaryNumber.cpp

--- 92B-BinaryNumber.cpp
+++ 92B-BinaryNumber.cpp
@@ -2,6 +2,35 @@
 #include <iostream>
 #include <string>
 
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    std::string s;
+    std::cin >> s;
+    int cnt = 0;
+    while (s != "1")
+    {
+        if (s.back() == '0')
+            s.pop_back();
+        else
+        {
+            int i = s.size() - 1;
+            while (i >= 0 && s[i] == '1')
+                --i;
+            if (i < 0)
+                s = "1" + std::string(s.size(), '0');
+            else
+                s[i] = '1', s = s.substr(0, i + 1) + std::string(s.size() - i - 1, '0');
+        }
+        ++cnt;
+    }
+    std::cout << cnt << std::endl;
+    return 0;
+}
+
 int main(){
 
     std::string inputString("1");
