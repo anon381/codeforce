ed1dda05fc9bdb147ad2c26661f5d787fbe25fe1
Yabets Maregn <yabetsanon@gmail.com>
2025-03-25 12:00:00 +0900

    Add step-by-step comments to 92B-BinaryNumber-OLD.cpp

--- 92B-BinaryNumber-OLD.cpp
+++ 92B-BinaryNumber-OLD.cpp
@@ -6,25 +6,33 @@ using namespace std;
 
 int main()
 {
+    // Read the binary string
     string s;
     cin >> s;
     int cnt = 0;
+    // Repeat until the string becomes "1"
     while (s != "1")
     {
+        // If the last character is '0', remove it
         if (s.back() == '0')
             s.pop_back();
         else
         {
+            // Find the rightmost '1' sequence
             int i = s.size() - 1;
             while (i >= 0 && s[i] == '1')
                 --i;
+            // If all are '1', set to "1" followed by zeros
             if (i < 0)
                 s = "1" + string(s.size(), '0');
             else
+                // Otherwise, flip the rightmost '0' to '1' and set the rest to '0'
                 s[i] = '1', s = s.substr(0, i + 1) + string(s.size() - i - 1, '0');
         }
+        // Increment step count
         ++cnt;
     }
+    // Output the number of steps
     cout << cnt << endl;
     return 0;
 }
