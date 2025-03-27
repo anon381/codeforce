8291b2df7b0b4504412d0cde9a6abbf03b58cd1d
Yabets Maregn <yabetsanon@gmail.com>
2025-03-27 12:00:00 +0900

    Update 94A-RestoringPassword.cpp

--- 94A-RestoringPassword.cpp
+++ 94A-RestoringPassword.cpp
@@ -1,23 +1,27 @@
-#include <cstdio>
-#include <iostream>
-#include <string>
-#include <map>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-
-    const int N = 10; const int D = 10; const int L = 8;
-    std::string input; getline(std::cin, input);
-    std::map<std::string, int> digits;
-    for(int k = 0; k < N; k++){
-        std::string temp; getline(std::cin, temp);
-        digits.insert(std::pair<std::string, int>(temp, k));
+int main()
+{
+    string s;
+    cin >> s;
+    int cnt = 0;
+    while (s != "1")
+    {
+        if (s.back() == '0')
+            s.pop_back();
+        else
+        {
+            int i = s.size() - 1;
+            while (i >= 0 && s[i] == '1')
+                --i;
+            if (i < 0)
+                s = "1" + string(s.size(), '0');
+            else
+                s[i] = '1', s = s.substr(0, i + 1) + string(s.size() - i - 1, '0');
+        }
+        ++cnt;
     }
-
-    for(int k = 0; k < L; k++){
-        std::string temp = input.substr(k * D, D);
-        std::cout << digits[temp];
-    }
-    puts("");
-
+    cout << cnt << endl;
     return 0;
 }
