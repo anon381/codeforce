09fb574460f069e77bec184acb295062b7024712
Yabets Maregn <yabetsanon@gmail.com>
2025-03-28 12:00:00 +0900

    Update 94B-Friends.cpp

--- 94B-Friends.cpp
+++ 94B-Friends.cpp
@@ -1,23 +1,17 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main()
+{
 
-    int n(0); scanf("%d", &n);
-
-    bool output(1);
-
-    if(n == 5){
-        output = 0;
-
-        int array[5] = {0};
-        for(int k = 0; k < n; k++){
-            int a(0), b(0); scanf("%d %d", &a, &b);
-            ++array[a - 1]; ++ array[b - 1];
-        }
-        for(int k = 0; k < 5; k++){if(array[k] != 2){output = 1; break;}}
-    }
-
-    if(output){puts("WIN");} else{puts("FAIL");}
-
+    int n(0);
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+    int ans = 0;
+    for (int i = 0; i < n; ++i)
+        ans = max(ans, a[i]);
+    cout << ans << endl;
     return 0;
 }
