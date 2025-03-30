fa9ca046f001b002e8cfd7c1c1f8533ef8592ab6
Yabets Maregn <yabetsanon@gmail.com>
2025-03-30 12:00:00 +0900

    Add step-by-step comments to 94B-Friends.cpp

--- 94B-Friends.cpp
+++ 94B-Friends.cpp
@@ -6,15 +6,18 @@ using namespace std;
 
 int main()
 {
-
+    // Read the number of friends
     int n(0);
     cin >> n;
+    // Read each friend's score
     vector<int> a(n);
     for (int i = 0; i < n; ++i)
         cin >> a[i];
     int ans = 0;
+    // Find the maximum score
     for (int i = 0; i < n; ++i)
         ans = max(ans, a[i]);
+    // Output the maximum score
     cout << ans << endl;
     return 0;
 }
