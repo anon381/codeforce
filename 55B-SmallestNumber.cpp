5bb4e1d381bf3f41041462f52f91d1f2b01eb769
Yabets Maregn <yabetsanon@gmail.com>
2025-06-27 12:00:00 +0900

    Add 55B-SmallestNumber.cpp

--- 55B-SmallestNumber.cpp
+++ 55B-SmallestNumber.cpp
@@ -1,12 +1,11 @@
-#include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main(){
 
     const int N = 4;
-    std::vector<long> num(N); scanf("%ld %ld %ld %ld\n", &num[0], &num[1], &num[2], &num[3]);
-    std::vector<char> op(N - 1); scanf("%c %c %c\n", &op[0], &op[1], &op[2]);
+    vector<long> num(N); scanf("%ld %ld %ld %ld\n", &num[0], &num[1], &num[2], &num[3]);
+    vector<char> op(N - 1); scanf("%c %c %c\n", &op[0], &op[1], &op[2]);
     long long output(1e13);
     sort(num.begin(), num.end());
 
@@ -28,7 +27,7 @@ int main(){
         if(op[2] == '+'){current += num[0];} else{current *= num[0];}
         if(current < output){output = current;}
 
-    }while(std::next_permutation(num.begin(), num.end()));
+    }while(next_permutation(num.begin(), num.end()));
 
     printf("%lld\n", output);
 
