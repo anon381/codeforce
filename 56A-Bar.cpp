7b8c1f8f9fa405f218b9aa3438c985389d086caf
Yabets Maregn <yabetsanon@gmail.com>
2025-06-30 12:00:00 +0900

    Add 56A-Bar.cpp

--- 56A-Bar.cpp
+++ 56A-Bar.cpp
@@ -1,14 +1,14 @@
-#include <cstdio>
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int convertToAge(std::string str){
+int convertToAge(string str){
 
     int output(0);
     for(int k = 0; k < str.size(); k++){output = 10 * output + (str[k] - '0');}
     return output;
 }
 
-bool isAlcoholicDrink(std::string drink){
+bool isAlcoholicDrink(string drink){
     return (drink == "ABSINTH" || drink == "BEER" || drink == "BRANDY" || drink == "CHAMPAGNE" || drink == "GIN" || drink == "RUM" || drink == "SAKE" || drink == "TEQUILA" || drink == "VODKA" || drink == "WHISKEY" || drink == "WINE");
 }
 
@@ -16,11 +16,11 @@ bool isAlcoholicDrink(std::string drink){
 int main(){
 
     int n(0); scanf("%d\n", &n);
-    std::string input("");
+    string input("");
 
     long total(0);
     for(int k = 0; k < n; k++){
-        getline(std::cin, input);
+    getline(cin, input);
         if(input[0] >= '0' && input[0] <= '9'){if(convertToAge(input) < 18){++total;}}
         else if(isAlcoholicDrink(input)){++total;}
     }
