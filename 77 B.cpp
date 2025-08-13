a93b1f18f2f7be2d69062e8ccda6ae8f2d7e5f8d
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-13 19:58:31 +0900

    Update 77 B.cpp

--- 77 B.cpp
+++ 77 B.cpp
@@ -1,7 +1,5 @@
-#include <cstdio>
-
+#include <bits/stdc++.h>
 int main(){
-
     long t; scanf("%ld", &t);
     while(t--){
         double a, b; scanf("%lf %lf", &a, &b);
@@ -9,9 +7,7 @@ int main(){
         if(b <= 0){prob = 1.0;}
         else if(a <= 0){prob = 0.5;}
         else{prob = (a < 4 * b) ? (0.5 + a /(16 * b)) : (1 - b / a);}
-
         printf("%.7lf\n", prob);
     }
-
     return 0;
 }
