2b19874b13a9a3b5947fe3ec51979bdb08fddb8c
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-15 19:22:13 +0900

    Update 62A-AStudentsDream.cpp

--- 62A-AStudentsDream.cpp
+++ 62A-AStudentsDream.cpp
@@ -1,2 +1,14 @@
 
 #include <bits/stdc++.h>
+
+
+int main(){
+
+    int girlLeft(0), girlRight(0); scanf("%d %d", &girlLeft, &girlRight);
+    int boyLeft(0), boyRight(0); scanf("%d %d", &boyLeft, &boyRight);
+
+    if((girlLeft - 1 <= boyRight && boyRight <= 2 * (girlLeft + 1)) || (girlRight - 1 <= boyLeft && boyLeft <= 2 * (girlRight + 1)) ){puts("YES");}
+    else{puts("NO");}
+
+    return 0;
+}
