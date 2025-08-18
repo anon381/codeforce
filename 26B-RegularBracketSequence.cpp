ce30a3f3df629651036a090ac9ed234c730488ad
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:09:26 +0900

    Update 26B-RegularBracketSequence.cpp

--- 26B-RegularBracketSequence.cpp
+++ 26B-RegularBracketSequence.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
-    std::string input; getline(std::cin, input);
+    string input; 
+    getline(cin, input);
 
     long unmatched(0), matched(0);
 
-    for(int k = 0; k < input.size(); k++){
-        if(input[k] == '('){++unmatched;}
-        else if(input[k] == ')' && unmatched > 0){--unmatched; ++matched;}
+    for(int k = 0; k < input.size(); k++) {
+        if(input[k] == '(') { ++unmatched; }
+        else if(input[k] == ')' && unmatched > 0) { --unmatched; ++matched; }
     }
 
-    std::cout << 2 * matched << std::endl;
+    cout << 2 * matched << endl;
 
     return 0;
 }
