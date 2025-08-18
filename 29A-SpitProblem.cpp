e70981b937aacf8498e76f8e5edbdb255e6e085a
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:05:22 +0900

    Update 29A-SpitProblem.cpp

--- 29A-SpitProblem.cpp
+++ 29A-SpitProblem.cpp
@@ -3,12 +3,9 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
 int main() {
-
     long n(0); 
     scanf("%ld", &n);
-
     vector<pair<long, long>> events;
     for(long k = 0; k < n; k++) {
         long pos(0), spit(0); 
@@ -19,9 +16,7 @@ int main() {
         }
         events.push_back(pair<long,long>(pos, spit));
     }
-
     sort(events.begin(), events.end());
-
     bool mutual(0);
     for(long k = 0; k < n - 1; k++) {
         if(events[k].first == events[k+1].first && events[k].second == events[k+1].second) {
@@ -29,13 +24,11 @@ int main() {
             break;
         }
     }
-
     if(mutual) { 
         puts("YES"); 
     } 
     else { 
         puts("NO"); 
     }
-
     return 0;
 }
