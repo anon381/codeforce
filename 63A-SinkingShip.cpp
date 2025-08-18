868ae8b939f29d150a908e33a3bbba8d4a81211d
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:19:39 +0900

    Update 63A-SinkingShip.cpp

--- 63A-SinkingShip.cpp
+++ 63A-SinkingShip.cpp
@@ -1,33 +1,29 @@
-
 #include <bits/stdc++.h>
-
+using namespace std;
 
 int main(){
-
-    int n; scanf("%d\n", &n);
+    int n; 
+    scanf("%d\n", &n);
     
-    std::string line("");
-    std::string shipCaptain("");
-    std::vector<std::string> rats;
-    std::vector<std::string> womenAndChildren;
-    std::vector<std::string> men;
+    string line, shipCaptain;
+    vector<string> rats, womenAndChildren, men;
 
     while(n--){
-        getline(std::cin, line);
+        getline(cin, line);
         int split = line.find(' ');
-        std::string name  = line.substr(0,split);
-        std::string title = line.substr(split + 1);
+        string name  = line.substr(0, split);
+        string title = line.substr(split + 1);
 
-        if(title == "captain"){shipCaptain = name;}
-        else if(title == "rat"){rats.push_back(name);}
-        else if(title == "woman" || title == "child"){womenAndChildren.push_back(name);}
-        else{men.push_back(name);}
+        if(title == "captain") shipCaptain = name;
+        else if(title == "rat") rats.push_back(name);
+        else if(title == "woman" || title == "child") womenAndChildren.push_back(name);
+        else men.push_back(name);
     }
 
-    for(int k = 0; k < rats.size(); k++){std::cout << rats[k] << std::endl;}
-    for(int k = 0; k < womenAndChildren.size(); k++){std::cout << womenAndChildren[k] << std::endl;}
-    for(int k = 0; k < men.size(); k++){std::cout << men[k] << std::endl;}
-    std::cout << shipCaptain << std::endl;
+    for(const auto &r : rats) cout << r << endl;
+    for(const auto &wc : womenAndChildren) cout << wc << endl;
+    for(const auto &m : men) cout << m << endl;
+    cout << shipCaptain << endl;
 
     return 0;
 }
