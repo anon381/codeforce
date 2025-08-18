1b54c795ecefebb6e0ffc29b98029318697ebc62
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:14:24 +0900

    Update 20A-BerOSFileSystem.cpp

--- 20A-BerOSFileSystem.cpp
+++ 20A-BerOSFileSystem.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
-int main(){
+using namespace std;
 
-    std::string path; getline(std::cin, path);
-    std::string output; bool flag(0);
-    for(int k = 0; k < path.size(); k++){
-        if(path[k] != '/' || flag == 0){output += path[k];}
-        if(path[k] == '/'){flag = 1;}
-        else{flag = 0;}
+int main() {
+
+    string path; 
+    getline(cin, path);
+
+    string output; 
+    bool flag(0);
+
+    for(int k = 0; k < path.size(); k++) {
+        if(path[k] != '/' || flag == 0) { output += path[k]; }
+        if(path[k] == '/') { flag = 1; }
+        else { flag = 0; }
     }
 
-    if(flag && output.size() > 1){output = output.substr(0, output.size() - 1);}
-    std::cout << output << std::endl;
+    if(flag && output.size() > 1) { output = output.substr(0, output.size() - 1); }
+
+    cout << output << endl;
 
     return 0;
 }
