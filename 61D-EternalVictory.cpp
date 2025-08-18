a6eb3935ad2dac81cdb742ce8c58869932b4bbfb
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:18:57 +0900

    Update 61D-EternalVictory.cpp

--- 61D-EternalVictory.cpp
+++ 61D-EternalVictory.cpp
@@ -1,41 +1,40 @@
-
 #include <bits/stdc++.h>
+using namespace std;
 
-void dfs(int64_t node, const std::vector<std::vector<std::pair<int64_t, int64_t> > > &g, std::vector<bool> &visited, std::vector<int64_t> &distance){
-
-    if(visited[node]){return;}
-    visited[node] = 1;
+void dfs(int64_t node, const vector<vector<pair<int64_t, int64_t>>> &g, vector<bool> &visited, vector<int64_t> &distance){
+    if(visited[node]) return;
+    visited[node] = true;
 
     for(int64_t p = 0; p < g[node].size(); p++){
         int64_t u = g[node][p].first;
-        if(visited[u]){continue;}
+        if(visited[u]) continue;
         distance[u] = distance[node] + g[node][p].second;
         dfs(u, g, visited, distance);
     }
-
-    return;
 }
 
-
 int main(){
-
     int64_t n; scanf("%lld", &n);
-    std::vector<std::vector<std::pair<int64_t, int64_t> > > g(n);
-    int64_t total(0);
+    vector<vector<pair<int64_t, int64_t>>> g(n);
+    int64_t total = 0;
+
     for(int64_t p = 1; p < n; p++){
         int64_t x, y, d; scanf("%lld %lld %lld", &x, &y, &d);
         --x; --y;
-        g[x].push_back(std::make_pair(y, d));
-        g[y].push_back(std::make_pair(x, d));
+        g[x].push_back({y, d});
+        g[y].push_back({x, d});
         total += 2 * d;
     }
 
-    std::vector<bool> mark(n, 0);
-    std::vector<int64_t> dist(n, 0);
+    vector<bool> mark(n, false);
+    vector<int64_t> dist(n, 0);
     dfs(0, g, mark, dist);
-    int64_t maxDist(0);
-    for(int64_t p = 0; p < n; p++){maxDist = (maxDist > dist[p]) ? maxDist : dist[p];} 
-    printf("%lld\n", total - maxDist);
 
+    int64_t maxDist = 0;
+    for(int64_t p = 0; p < n; p++){
+        maxDist = max(maxDist, dist[p]);
+    }
+
+    printf("%lld\n", total - maxDist);
     return 0;
 }
