9ebac0d5a7596a6f153b1c454c1aaa8646a0efd2
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:16:05 +0900

    Update F.cpp

--- Codeforces Round 1042 (Div. 3)/F.cpp
+++ Codeforces Round 1042 (Div. 3)/F.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
-
+using namespace std;
 
 #define all(a) (a).begin(), (a).end()
 #define endl "\n"
-#define vec std::vector
-#define pii std::pair<int, int>
+#define vec vector
+#define pii pair<int, int>
 #define se second
 #define fi first
 #define pb push_back
 #define maxel(v) *max_element(v.begin(), v.end())
 #define minel(v) *min_element(v.begin(), v.end())
-#define yes std::cout << "YES\n";
-#define m1 std::cout << "-1\n";
-#define no std::cout << "NO\n";
+#define yes cout << "YES\n";
+#define m1 cout << "-1\n";
+#define no cout << "NO\n";
+
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double ld;
+
 const ll mod = 1e9 + 7;
 const ll mod2 = 1e9 + 9;
 const ll mod3 = 998244353;
@@ -26,19 +28,19 @@ const int MAXN = 1e5 * 5;
 template<typename T>
 void printa(const vec<T>& a) {
     for (const auto& p : a) {
-        std::cout << p << ' ';
+        cout << p << ' ';
     }
-    std::cout << endl;
+    cout << endl;
 }
+
 template<typename T1, typename T2>
-std::istream& operator >> (std::istream& in, std::pair<T1, T2>& p){
-    in >> p.first;
-    in >> p.second;
+istream& operator >> (istream& in, pair<T1, T2>& p){
+    in >> p.first >> p.second;
     return in;
 }
 
 template<typename T>
-std::istream& operator >> (std::istream& in, vec<T>& v){
+istream& operator >> (istream& in, vec<T>& v){
     for (auto& e : v) in >> e;
     return in;
 }
@@ -47,17 +49,17 @@ template <typename T>
 void print2d(const vec<vec<T>>& v) { 
     for (const auto& row : v) {     
         for (const auto& i : row) {
-            std::cout << i << " ";
+            cout << i << " ";
         }
-        std::cout << endl;
+        cout << endl;
     }
 }
 
 void solve() {
     ll n;
-    std::cin >> n;
-    std::string a, b;
-    std::cin >> a >> b;
+    cin >> n;
+    string a, b;
+    cin >> a >> b;
 
     vec<ll> pa(n + 1, 0);
     vec<ll> pb(n + 1, 0);
@@ -83,20 +85,20 @@ void solve() {
 
     ll abs_sum = 0;
     for (ll c : va) {
-        ll k = std::lower_bound(all(vb), -c) - vb.begin();
+        ll k = lower_bound(all(vb), -c) - vb.begin();
         abs_sum += c * (n - 2 * k) + tot_sum_vb - 2 * p_sum_vb[k];
     }
 
     ll tot_xy_sum = n * n * (n + 1);
     ll ans = (tot_xy_sum - abs_sum) / 2;
-    std::cout << ans << endl;
+    cout << ans << endl;
 }
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
-    std::cin >> t;
+    cin >> t;
     while (t--) {
         solve();
     }
