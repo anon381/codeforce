3fc6b8588fc097280f85740b3d0e7f70cc5e1602
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:12:46 +0900

    Update 23A-YoureGivenAString.cpp

--- 23A-YoureGivenAString.cpp
+++ 23A-YoureGivenAString.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
-int main(){
+using namespace std;
 
-    std::string input; getline(std::cin, input);
+int main() {
+
+    string input; 
+    getline(cin, input);
     int output(0);
 
-    for(size_t length = input.size() - 1; length >= 1; length--){
-        if(output > 0){break;}
-        std::set<std::string> present;
+    for(size_t length = input.size() - 1; length >= 1; length--) {
+        if(output > 0) { break; }
+        set<string> present;
 
-        for(size_t start = 0; start < input.size() - length + 1; start++){
-            std::string current = input.substr(start, length);
-            if(present.find(current) == present.end()){present.insert(current);}
-            else{output = length; break;}
+        for(size_t start = 0; start < input.size() - length + 1; start++) {
+            string current = input.substr(start, length);
+            if(present.find(current) == present.end()) { 
+                present.insert(current); 
+            } 
+            else { 
+                output = length; 
+                break; 
+            }
         }
     }
 
-    std::cout << output << std::endl;
+    cout << output << endl;
 
     return 0;
 }
