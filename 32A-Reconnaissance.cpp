7113a3d64e230e8cd5f5085b9f4751644ec7314c
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 20:59:13 +0900

    Update 32A-Reconnaissance.cpp

--- 32A-Reconnaissance.cpp
+++ 32A-Reconnaissance.cpp
@@ -1,14 +1,11 @@
 #include <cstdio>
 #include <algorithm>
 using namespace std;
-
 int main() {
-
     long n(0), d(0); scanf("%ld %ld", &n, &d);
     long * height = new long[n];
     for(int k = 0; k < n; k++){scanf("%ld", height + k);}
     sort(height, height + n);
-
     long total(0);
     for(int k = 0; k < n; k++){
         for(int m = k + 1; m < n; m++){
@@ -16,9 +13,7 @@ int main() {
             else{break;} 
         }
     }
-
     printf("%ld\n", 2 * total);
-
     delete[] height;
     return 0;
 }
