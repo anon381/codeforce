fd0201187c103f5ce3c4ca4adf02329cf9e356c2
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 20:56:55 +0900

    Update 32B-Borze.cpp

--- 32B-Borze.cpp
+++ 32B-Borze.cpp
@@ -1,10 +1,7 @@
 #include <cstdio>
 #include <iostream>
-
 int main(){
-
     std::string input; getline(std::cin, input);
-
     size_t index = 0;
     while(index < input.size()){
         if(input[index] == '.'){printf("0");}
@@ -12,6 +9,5 @@ int main(){
         else if(input[index]== '-' && input[index + 1] == '-'){printf("2"); ++index;}
         ++index;
     }
-
     return 0;
 }
