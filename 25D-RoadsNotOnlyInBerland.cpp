b237da6efcf3f161f2a9b174afaa748f400ca979
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:10:31 +0900

    Update 25D-RoadsNotOnlyInBerland.cpp

--- 25D-RoadsNotOnlyInBerland.cpp
+++ 25D-RoadsNotOnlyInBerland.cpp
@@ -1,29 +1,43 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+long find(long x, vector<long> &v) {
+    return (v[x] == x) ? x : (v[x] = find(v[x], v));
+}
 
-long find(long x, std::vector<long> &v){return (v[x] == x) ? x : (v[x] = find(v[x], v));}
+int main() {
 
-int main(){
+    long n; 
+    scanf("%ld", &n);
+    
+    vector<long> f(n + 1, 0); 
+    for(long p = 1; p <= n; p++) { f[p] = p; }
 
-    long n; scanf("%ld", &n);
-    std::vector<long> f(n + 1, 0); for(long p = 1; p <= n; p++){f[p] = p;}
-    std::vector<std::pair<long, long> > rem;
-    for(long p = 1; p < n; p++){
-        long x, y; scanf("%ld %ld", &x, &y);
-        long r = find(x, f); long s = find(y, f);
-        if(r == s){rem.push_back(std::make_pair(x, y));}
-        else{f[s] = r;}
+    vector<pair<long, long>> rem;
+    for(long p = 1; p < n; p++) {
+        long x, y; 
+        scanf("%ld %ld", &x, &y);
+        long r = find(x, f); 
+        long s = find(y, f);
+        if(r == s) { 
+            rem.push_back(make_pair(x, y)); 
+        } 
+        else { 
+            f[s] = r; 
+        }
     }
 
-    std::vector<std::pair<long, long> > add;
-    for(long p = 2; p <= n; p++){
-        if(find(p, f) == find(1, f)){continue;}
-        add.push_back(std::make_pair(find(p, f), 1));
+    vector<pair<long, long>> add;
+    for(long p = 2; p <= n; p++) {
+        if(find(p, f) == find(1, f)) { continue; }
+        add.push_back(make_pair(find(p, f), 1));
         f[find(p, f)] = find(1, f);
     }
 
     printf("%ld\n", rem.size());
-    for(long p = 0; p < rem.size(); p++){printf("%ld %ld %ld %ld\n", rem[p].first, rem[p].second, add[p].first, add[p].second);}
+    for(long p = 0; p < rem.size(); p++) {
+        printf("%ld %ld %ld %ld\n", rem[p].first, rem[p].second, add[p].first, add[p].second);
+    }
 
     return 0;
 }
