47a09eda7d64ebdccd4bc8dd77df72e38219cd57
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:17:32 +0900

    Update 60A-WhereAreMyFlakes.cpp

--- 60A-WhereAreMyFlakes.cpp
+++ 60A-WhereAreMyFlakes.cpp
@@ -1,26 +1,33 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-
-    int n(0), m(0); scanf("%d %d\n", &n, &m);
+int main() {
+    int n(0), m(0);
+    scanf("%d %d\n", &n, &m);
 
     int leftBoundary(1), rightBoundary(n);
 
-    while(m--){
+    while (m--) {
+        string hint;
+        getline(cin, hint);
 
-        std::string hint; getline(std::cin, hint);
+        int currentBox(0), index(hint.size() - 1);
 
-        int currentBox(0),index(hint.size() - 1);
+        while (hint[index] != ' ') --index;
+        while (++index < hint.size()) currentBox = 10 * currentBox + (hint[index] - '0');
 
-        while(hint[index] != ' '){--index;}
-        while(++index < hint.size()){currentBox = 10 * currentBox + (hint[index] - '0');}
-
-        if(hint.find("left") != std::string::npos && rightBoundary > currentBox - 1){rightBoundary = currentBox - 1;}
-        else if(hint.find("right") != std::string::npos && leftBoundary < currentBox + 1){leftBoundary = currentBox + 1;}
+        if (hint.find("left") != string::npos && rightBoundary > currentBox - 1) {
+            rightBoundary = currentBox - 1;
+        } else if (hint.find("right") != string::npos && leftBoundary < currentBox + 1) {
+            leftBoundary = currentBox + 1;
+        }
     }
 
-    if(rightBoundary < leftBoundary){puts("-1");}
-    else{std::cout << rightBoundary - leftBoundary + 1 << std::endl;}
+    if (rightBoundary < leftBoundary) {
+        puts("-1");
+    } else {
+        cout << rightBoundary - leftBoundary + 1 << endl;
+    }
 
     return 0;
 }
