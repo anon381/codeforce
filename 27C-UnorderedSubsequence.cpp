76b23d3af1beda4f2f77ca07d3ecfeec75e8bc38
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:08:09 +0900

    Update 27C-UnorderedSubsequence.cpp

--- 27C-UnorderedSubsequence.cpp
+++ 27C-UnorderedSubsequence.cpp
@@ -1,19 +1,29 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
-    long n; scanf("%ld", &n);
-    if(n <= 2){puts("0"); return 0;}
+    long n; 
+    scanf("%ld", &n);
+    if(n <= 2) { puts("0"); return 0; }
 
-    std::vector<long long> a(n); for(long p = 0; p < n; p++){scanf("%lld", &a[p]);}
+    vector<long long> a(n); 
+    for(long p = 0; p < n; p++) { scanf("%lld", &a[p]); }
+
     long ind(-1);
-    for(long p = 0; p + 1 < n; p++){
-        if((a[p + 1] - a[p]) * (a[p] - a[0]) < 0){ind = p; break;}
+    for(long p = 0; p + 1 < n; p++) {
+        if((a[p + 1] - a[p]) * (a[p] - a[0]) < 0) { 
+            ind = p; 
+            break; 
+        }
     }
 
-
-    if(ind < 0){puts("0");}
-    else{printf("3\n1 %ld %ld\n", ind + 1, ind + 2);}
+    if(ind < 0) { 
+        puts("0"); 
+    } 
+    else { 
+        printf("3\n1 %ld %ld\n", ind + 1, ind + 2); 
+    }
 
     return 0;
 }
