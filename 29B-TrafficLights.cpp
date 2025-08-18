9122953311a43e0560b0bdab956216575dfda991
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:03:48 +0900

    Update 29B-TrafficLights.cpp

--- 29B-TrafficLights.cpp
+++ 29B-TrafficLights.cpp
@@ -1,14 +1,20 @@
 #include <cstdio>
+using namespace std;
 
-int main(){
+int main() {
 
-    long l, d, v, g, r; scanf("%ld %ld %ld %ld %ld", &l, &d, &v, &g, &r);
+    long l, d, v, g, r; 
+    scanf("%ld %ld %ld %ld %ld", &l, &d, &v, &g, &r);
 
     double t1 = 1.0 * d / v;
     double t2 = 1.0 * (l - d) / v;
     
-    long u = t1 / (g + r); double tl = t1 - u * (g + r);
-    if(tl >= g){t1 = (u + 1) * (g + r);}
+    long u = t1 / (g + r); 
+    double tl = t1 - u * (g + r);
+
+    if(tl >= g) { 
+        t1 = (u + 1) * (g + r); 
+    }
 
     printf("%.7lf\n", t1 + t2);
 
