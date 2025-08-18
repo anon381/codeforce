3f56a5d027fd1333bb1b9650954eac2bf015c689
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-18 21:11:03 +0900

    Update 25B-PhoneNumbers.cpp

--- 25B-PhoneNumbers.cpp
+++ 25B-PhoneNumbers.cpp
@@ -1,23 +1,36 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+int main() {
 
-int main(){
+    int n(0); 
+    scanf("%d\n", &n);
+    
+    string number; 
+    getline(cin, number);
 
-    int n(0); scanf("%d\n", &n);
-    std::string number; getline(std::cin, number);
-    if(number.size() <= 3){std::cout << number << std::endl;}
-    else{
+    if(number.size() <= 3) {
+        cout << number << endl;
+    } 
+    else {
         int index(0);
 
-        if(number.size() % 2){std::cout << number.substr(0,3); index = 3;}
-        else{std::cout << number.substr(0,2); index = 2;}
+        if(number.size() % 2) {
+            cout << number.substr(0, 3); 
+            index = 3;
+        } 
+        else {
+            cout << number.substr(0, 2); 
+            index = 2;
+        }
 
-        while(index < number.size()){
-            std::cout << "-";
-            std::cout << number.substr(index, 2);
+        while(index < number.size()) {
+            cout << "-";
+            cout << number.substr(index, 2);
             index += 2;
         }
-        std::cout << std::endl;
+        cout << endl;
     }
+
     return 0;
 }
