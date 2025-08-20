e3f9781ca70fea64b7f7bf9147ee57bb0b85d44a
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-20 21:05:25 +0900

    Update 34C-PageNumbers.cpp

--- 34C-PageNumbers.cpp
+++ 34C-PageNumbers.cpp
@@ -1,41 +1,54 @@
 #include <bits/stdc++.h>
-std::vector<int> parseString(std::string s){
+using namespace std;
 
+vector<int> parseString(string s) {
     int x(0);
-    std::vector<int> nums;
-    for(long p = 0; p < s.size(); p++){
-        if(s[p] == ','){nums.push_back(x); x = 0;}
-        else{x = 10 * x + (s[p] - '0');}
+    vector<int> nums;
+    for (long p = 0; p < s.size(); p++) {
+        if (s[p] == ',') {
+            nums.push_back(x);
+            x = 0;
+        } else {
+            x = 10 * x + (s[p] - '0');
+        }
     }
     nums.push_back(x);
     return nums;
 }
 
-int main(){
-
+int main() {
     const long N = 1111;
-    std::ios_base::sync_with_stdio(false);
-    std::string s; std::cin >> s;
-    std::vector<int> nums = parseString(s);
-    std::vector<bool> a(N, 0);
-    for(long p = 0; p < nums.size(); p++){a[nums[p]] = true;}
+    ios_base::sync_with_stdio(false);
+    string s; 
+    cin >> s;
+    vector<int> nums = parseString(s);
+    vector<bool> a(N, 0);
 
-    long from(-1), initial(1);
-    for(long p = 0; p < N; p++){
-        if(!a[p]){
-            if(from >= 0){
-                long to = p - 1;
-                if(!initial){std::cout << ",";}
-                initial = false;
-                if(from < to){std::cout << from << "-" << to;}
-                else{std::cout << from;}
-            }
-            from = -1;
-        }
-        else if(a[p] && from < 0){from = p;}
+    for (long p = 0; p < nums.size(); p++) {
+        a[nums[p]] = true;
     }
 
-    std::cout << std::endl;
+    long from(-1), initial(1);
+    for (long p = 0; p < N; p++) {
+        if (!a[p]) {
+            if (from >= 0) {
+                long to = p - 1;
+                if (!initial) {
+                    cout << ",";
+                }
+                initial = false;
+                if (from < to) {
+                    cout << from << "-" << to;
+                } else {
+                    cout << from;
+                }
+            }
+            from = -1;
+        } else if (a[p] && from < 0) {
+            from = p;
+        }
+    }
 
+    cout << endl;
     return 0;
 }
