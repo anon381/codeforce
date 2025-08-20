83f07058bc6cc3b7546fd8427dbaf36e4a874dd5
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-20 21:10:02 +0900

    Update 75 C.cpp

--- 75 C.cpp
+++ 75 C.cpp
@@ -1,24 +1,39 @@
 #include <bits/stdc++.h>
-long gcd(long a, long b){return (b == 0) ? a : gcd (b, a%b);}
+using namespace std;
 
-int main(){
+long gcd(long a, long b) {
+    return (b == 0) ? a : gcd(b, a % b);
+}
 
-    long a, b; scanf("%ld %ld\n", &a, &b);
+int main() {
+    long a, b; 
+    scanf("%ld %ld\n", &a, &b);
+
     long g = gcd(a, b);
-    std::vector<long> div;
-    for(long p = 1; p * p <= g; p++){
-        if(g % p == 0){
+    vector<long> div;
+    for (long p = 1; p * p <= g; p++) {
+        if (g % p == 0) {
             div.push_back(p);
-            if(p * p < g){div.push_back(g / p);}
+            if (p * p < g) {
+                div.push_back(g / p);
+            }
         }
     }
     sort(div.begin(), div.end());
 
-    long q; scanf("%ld", &q);
-    while(q--){
-        long low, high; scanf("%ld %ld", &low, &high);
-        long mgcd = *(--(std::upper_bound(div.begin(), div.end(), high)));
-        printf("%ld\n", (mgcd >= low) ? mgcd : -1);
+    long q; 
+    scanf("%ld", &q);
+    while (q--) {
+        long low, high; 
+        scanf("%ld %ld", &low, &high);
+
+        auto it = upper_bound(div.begin(), div.end(), high);
+        if (it == div.begin()) {
+            printf("-1\n");
+        } else {
+            long mgcd = *(--it);
+            printf("%ld\n", (mgcd >= low) ? mgcd : -1);
+        }
     }
 
     return 0;
