6596f0101e51bd0fced9853c3aa116e83bb5339e
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-20 21:08:01 +0900

    Update 74 A.cpp

--- 74 A.cpp
+++ 74 A.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
-// Computes total score for each student based on given criteria.
-int main(){
+using namespace std;
 
-    int n; scanf("%d\n", &n);
-    std::map<std::string, int> scores;
-    for(int k = 0; k < n; k++){
-        char name[20]; scanf("%s", name);
+int main() {
+    int n; 
+    scanf("%d\n", &n);
+
+    map<string, int> scores;
+    for (int k = 0; k < n; k++) {
+        char name[20]; 
+        scanf("%s", name);
+
         int temp(0), total(0);
-        scanf("%d", &temp);total += 100 * temp;
-        scanf("%d", &temp);total -= 50 * temp;
-        for(int p = 0; p < 5; p++){scanf("%d", &temp);total += temp;}
+        scanf("%d", &temp); total += 100 * temp;
+        scanf("%d", &temp); total -= 50 * temp;
+
+        for (int p = 0; p < 5; p++) {
+            scanf("%d", &temp);
+            total += temp;
+        }
         scanf("\n");
-        scores.insert(std::pair<std::string,int>(std::string(name),total));
+
+        scores.insert(make_pair(string(name), total));
     }
 
-    std::map<std::string, int>::iterator myIter = scores.begin();
-    std::string roomLeader = myIter -> first;
-    int maxScore = myIter -> second;
+    map<string, int>::iterator myIter = scores.begin();
+    string roomLeader = myIter->first;
+    int maxScore = myIter->second;
 
-    for(myIter = scores.begin(); myIter != scores.end(); myIter++){
-        if((myIter -> second) > maxScore){
-            roomLeader = myIter -> first;
-            maxScore = myIter -> second;
+    for (myIter = scores.begin(); myIter != scores.end(); myIter++) {
+        if ((myIter->second) > maxScore) {
+            roomLeader = myIter->first;
+            maxScore = myIter->second;
         }
     }
 
-    std::cout << roomLeader << std::endl;
+    cout << roomLeader << endl;
 }
