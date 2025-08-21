4e9854db0ed949f933cee618bc896c516e3694a1
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-21 21:02:50 +0900

    Update 36-a-ExtraTerrestrialIntelligence.cpp

--- 36-a-ExtraTerrestrialIntelligence.cpp
+++ 36-a-ExtraTerrestrialIntelligence.cpp
@@ -1,30 +1,41 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-
-    FILE * inputFile = fopen("input.txt","r");
-    int n(0); fscanf(inputFile, "%d\n", &n);
+int main() {
+    FILE *inputFile = fopen("input.txt", "r");
+    int n(0);
+    fscanf(inputFile, "%d\n", &n);
 
     int lastSignal(-1), distance(-1);
     bool output(1);
 
-    for(int p = 0; p < n; p++){
-        char temp('0'); fscanf(inputFile, "%c", &temp);
-        if(temp == '1'){
-            if(lastSignal < 0){lastSignal = p;}
-            else if(distance < 0){distance = p - lastSignal; lastSignal = p;}
-            else if(p - lastSignal == distance){lastSignal = p;}
-            else if(p - lastSignal != distance){output = 0; break;}
+    for (int p = 0; p < n; p++) {
+        char temp('0');
+        fscanf(inputFile, "%c", &temp);
+        if (temp == '1') {
+            if (lastSignal < 0) {
+                lastSignal = p;
+            } else if (distance < 0) {
+                distance = p - lastSignal;
+                lastSignal = p;
+            } else if (p - lastSignal == distance) {
+                lastSignal = p;
+            } else if (p - lastSignal != distance) {
+                output = 0;
+                break;
+            }
 
             printf("%d %d %d\n", p, lastSignal, distance);
-
         }
     }
     fclose(inputFile);
 
-    FILE * outputFile = fopen("output.txt","w");
-    if(output){fprintf(outputFile, "YES\n");}
-    else{fprintf(outputFile, "NO\n");}
+    FILE *outputFile = fopen("output.txt", "w");
+    if (output) {
+        fprintf(outputFile, "YES\n");
+    } else {
+        fprintf(outputFile, "NO\n");
+    }
     fclose(outputFile);
 
     return 0;
