886337e72d6d18c6ef3bfa85f5614c69468fd397
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-21 21:03:17 +0900

    Update 36D-NewGameWithAChessPiece.cpp

--- 36D-NewGameWithAChessPiece.cpp
+++ 36D-NewGameWithAChessPiece.cpp
@@ -1,20 +1,27 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-bool firstWins(long n, long m, long k){
-
-    long min = (m < n) ? m : n;
-    if(min % (k + 1) == 0){return true;}
-    else if(k == 1 || (min / (k + 1)) % 2 == 0){return (m % 2) != (n % 2);}
-    else{return (m % 2) == (n % 2);}
+bool firstWins(long n, long m, long k) {
+    long mn = (m < n) ? m : n;
+    if (mn % (k + 1) == 0) {
+        return true;
+    } else if (k == 1 || (mn / (k + 1)) % 2 == 0) {
+        return (m % 2) != (n % 2);
+    } else {
+        return (m % 2) == (n % 2);
+    }
 }
 
-int main(){
+int main() {
+    ifstream instream("input.txt");
+    FILE *outputFile = fopen("output.txt", "w");
 
-    std::ifstream instream; instream.open("input.txt");
-    FILE * outputFile = fopen("output.txt","w");
-    long t, k; instream >> t >> k;
-    while(t--){
-        long n, m; instream >> n >> m;
+    long t, k;
+    instream >> t >> k;
+
+    while (t--) {
+        long n, m;
+        instream >> n >> m;
         fputs(firstWins(n, m, k) ? "+\n" : "-\n", outputFile);
     }
 
