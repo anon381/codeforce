cb47cc013d163c9326d67110f68a6a9d58c7d16e
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-23 20:30:54 +0900

    Update 41B-MartianDollar.cpp

--- 41B-MartianDollar.cpp
+++ 41B-MartianDollar.cpp
@@ -1,18 +1,23 @@
-
 #include <cstdio>
 #include <vector>
+using namespace std;
 
-int main(){
+int main() {
 
-    int n, b; scanf("%d %d\n", &n, &b);
-    std::vector<int> price(n);
-    for(int p = 0; p < n; p++){scanf("%d\n", &price[p]);}
+    int n, b; 
+    scanf("%d %d\n", &n, &b);
+    vector<int> price(n);
+    for(int p = 0; p < n; p++) {
+        scanf("%d\n", &price[p]);
+    }
 
     int ans(b);
-    for(int u = 0; u < n; u++){
-        for(int v = u + 1; v < n; v++){
+    for(int u = 0; u < n; u++) {
+        for(int v = u + 1; v < n; v++) {
             int candidate = b - (b / price[u]) * price[u] + (b / price[u]) * price[v];
-            if(candidate > ans){ans = candidate;}
+            if(candidate > ans) {
+                ans = candidate;
+            }
         }
     }
 
