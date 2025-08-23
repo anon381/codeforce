0b2965ac040791785b44b3f424a204a40db8c2c9
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-23 19:23:13 +0900

    Update 39F-PacifistFrogs.cpp

--- 39F-PacifistFrogs.cpp
+++ 39F-PacifistFrogs.cpp
@@ -1,20 +1,30 @@
 #include <cstdio>
 #include <vector>
 #include <map>
+using namespace std;
 
-int main(){
+int main() {
 
     long n, m, k; scanf("%ld %ld %ld", &n, &m, &k);
-    std::vector<long> fv(m); for(long p = 0; p < m; p++){scanf("%ld", &fv[p]);}
-    std::vector<long> mv(k); for(long p = 0; p < k; p++){scanf("%ld", &mv[p]);}
+    vector<long> fv(m); 
+    for(long p = 0; p < m; p++){scanf("%ld", &fv[p]);}
+    vector<long> mv(k); 
+    for(long p = 0; p < k; p++){scanf("%ld", &mv[p]);}
 
-    long minSmashed(k + 1); std::vector<long> frogs;
+    long minSmashed(k + 1); 
+    vector<long> frogs;
 
     for(long p = 0; p < m; p++){
         long cnt(0);
         for(long q = 0; q < k; q++){cnt += (mv[q] % fv[p] == 0);}
-        if(cnt < minSmashed){minSmashed = cnt; frogs.clear(); frogs.push_back(p + 1);}
-        else if(cnt == minSmashed){frogs.push_back(p + 1);}
+        if(cnt < minSmashed){
+            minSmashed = cnt; 
+            frogs.clear(); 
+            frogs.push_back(p + 1);
+        }
+        else if(cnt == minSmashed){
+            frogs.push_back(p + 1);
+        }
     }
 
     printf("%ld\n", frogs.size());
