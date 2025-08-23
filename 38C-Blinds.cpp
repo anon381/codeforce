fd0ad96c13729194c841ecafaffd47d96c1b65aa
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-23 19:18:28 +0900

    Refactor main function and vector initialization
    Refactor main function and improve vector declaration.

--- 38C-Blinds.cpp
+++ 38C-Blinds.cpp
@@ -1,12 +1,14 @@
 #include <cstdio>
 #include <vector>
+using namespace std;
 
-int main(){
+int main() {
 
     const long M = 101;
     long n, l; scanf("%ld %ld", &n, &l);
-    std::vector<long> a(n);
+    vector<long> a(n);
     for(long p = 0; p < n; p++){scanf("%ld", &a[p]);}
+
     long maxArea(0);
     for(long u = l; u < M; u++){
         long area(0);
