2a2c8f655cb42cedb7e51692e5247044a3f36e7e
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-24 21:47:40 +0900

    Update 43B-Letter.cpp

--- 43B-Letter.cpp
+++ 43B-Letter.cpp
@@ -1,28 +1,43 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-
-int main(){
+int main() {
 
     const int N = 26;
-    std::vector<int> small(N,0);
-    std::vector<int> large(N,0);
+    vector<int> small(N, 0);
+    vector<int> large(N, 0);
 
-    std::string input; getline(std::cin, input);
-    for(int k = 0; k < input.size(); k++){
-        if('a' <= input[k] && input[k] <= 'z'){++small[input[k] - 'a'];}
-        if('A' <= input[k] && input[k] <= 'Z'){++large[input[k] - 'A'];}
+    string input; 
+    getline(cin, input);
+    for (int k = 0; k < input.size(); k++) {
+        if ('a' <= input[k] && input[k] <= 'z') {
+            ++small[input[k] - 'a'];
+        }
+        if ('A' <= input[k] && input[k] <= 'Z') {
+            ++large[input[k] - 'A'];
+        }
     }
 
-    std::string message; getline(std::cin, message);
-    for(int k = 0; k < message.size(); k++){
-        if('a' <= message[k] && message[k] <= 'z'){--small[message[k] - 'a'];}
-        if('A' <= message[k] && message[k] <= 'Z'){--large[message[k] - 'A'];}
+    string message; 
+    getline(cin, message);
+    for (int k = 0; k < message.size(); k++) {
+        if ('a' <= message[k] && message[k] <= 'z') {
+            --small[message[k] - 'a'];
+        }
+        if ('A' <= message[k] && message[k] <= 'Z') {
+            --large[message[k] - 'A'];
+        }
     }
 
-    std::string output = "YES";
-    for(int k = 0; k < N; k++){if(small[k] < 0 || large[k] < 0){output = "NO"; break;}}
+    string output = "YES";
+    for (int k = 0; k < N; k++) {
+        if (small[k] < 0 || large[k] < 0) {
+            output = "NO";
+            break;
+        }
+    }
 
-    std::cout << output << std::endl;
+    cout << output << endl;
 
     return 0;
 }
