c2151e6d6950325c50b3df7e3cd0444061032a7c
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-24 21:48:11 +0900

    Update 43C-LuckyTickets.cpp

--- 43C-LuckyTickets.cpp
+++ 43C-LuckyTickets.cpp
@@ -1,18 +1,28 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
-    long n; scanf("%ld\n", &n);
+    long n; 
+    scanf("%ld\n", &n);
+
     long zeros(0), ones(0), twos(0);
 
-    while(n--){
-        long t; scanf("%ld", &t);
-        if(t % 3 == 0){++zeros;}
-        else if(t % 3 == 1){++ones;}
-        else if(t % 3 == 2){++twos;}
+    while (n--) {
+        long t; 
+        scanf("%ld", &t);
+        if (t % 3 == 0) {
+            ++zeros;
+        }
+        else if (t % 3 == 1) {
+            ++ones;
+        }
+        else if (t % 3 == 2) {
+            ++twos;
+        }
     }
 
-    long total = (zeros / 2) + (ones < twos ? ones : twos);
+    long total = (zeros / 2) + min(ones, twos);
     printf("%ld\n", total);
 
     return 0;
