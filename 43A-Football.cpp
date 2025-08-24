c937ab509549f4166ac010eefee10f5eb865ac1f
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-24 21:47:12 +0900

    Update 43A-Football.cpp

--- 43A-Football.cpp
+++ 43A-Football.cpp
@@ -1,19 +1,27 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
-    int n; scanf("%d\n", &n);
-    std::string first(""), second(""), temp("");
+    int n; 
+    scanf("%d\n", &n);
+
+    string first(""), second(""), temp("");
     int goals[2] = {0};
 
-    while(n--){
-        getline(std::cin, temp);
-        if(first == ""){first = temp;}
-        else if(temp != first && second == ""){second = temp;}
+    while(n--) {
+        getline(cin, temp);
+        if(first == "") {
+            first = temp;
+        }
+        else if(temp != first && second == "") {
+            second = temp;
+        }
         (temp == first) ? ++goals[0] : ++goals[1];
     }
 
-    temp = (goals[0] > goals[1]) ? first : second ;
-    std::cout << temp << std::endl;
+    temp = (goals[0] > goals[1]) ? first : second;
+    cout << temp << endl;
+
     return 0;
 }
