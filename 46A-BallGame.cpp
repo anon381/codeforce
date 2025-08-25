71bbef41f7665305cf0fade5832f5145625c4693
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-25 18:28:22 +0900

    Update 46A-BallGame.cpp

--- 46A-BallGame.cpp
+++ 46A-BallGame.cpp
@@ -1,11 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    int n; 
+    scanf("%d", &n);
 
-int main(){
-
-    int n; scanf("%d", &n);
     int current(0);
-    for(int k = 1; k < n; k++){current += k;current %= n;printf("%d ",current + 1);}
+    for (int k = 1; k < n; k++) {
+        current += k;
+        current %= n;
+        printf("%d ", current + 1);
+    }
     printf("\n");
+
     return 0;
 }
