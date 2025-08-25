59b6d2c1d78671197bc2f2439da51902e331fa34
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-25 18:29:49 +0900

    Update 47A-TriangularNumbers.cpp

--- 47A-TriangularNumbers.cpp
+++ 47A-TriangularNumbers.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
+    int n(0); 
+    scanf("%d", &n);
 
-    int n(0); scanf("%d", &n);
     int temp = sqrt(1 + 8 * n);
-    if(temp * temp == 1 + 8 * n){puts("YES");}
-    else{puts("NO");}
+    if (temp * temp == 1 + 8 * n) {
+        puts("YES");
+    } else {
+        puts("NO");
+    }
 
     return 0;
 }
