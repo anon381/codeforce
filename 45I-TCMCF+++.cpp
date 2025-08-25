02cf4d6d2c47521a923b9ab1ccdf9888f7c60431
Yabets Maregn <149154528+anon381@users.noreply.github.com>
2025-08-25 18:27:57 +0900

    Update 45I-TCMCF+++.cpp

--- 45I-TCMCF+++.cpp
+++ 45I-TCMCF+++.cpp
@@ -1,22 +1,42 @@
 #include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
+    long n; 
+    scanf("%ld", &n);
 
-    long n; scanf("%ld", &n);
-    std::vector<long> pos, neg; long z(0);
-    for(long p = 0; p < n; p++){
-        long x; scanf("%ld", &x);
-        if(x > 0){pos.push_back(x);}
-        else if(x < 0){neg.push_back(x);}
-        else if(x == 0){++z;}
+    vector<long> pos, neg; 
+    long z(0);
+
+    for (long p = 0; p < n; p++) {
+        long x; 
+        scanf("%ld", &x);
+        if (x > 0) { 
+            pos.push_back(x); 
+        }
+        else if (x < 0) { 
+            neg.push_back(x); 
+        }
+        else { 
+            ++z; 
+        }
     }
 
     sort(neg.rbegin(), neg.rend());
-    if(n == 1 && neg.size() == 1){printf("%ld\n", neg[0]);}
-    else if(pos.empty() && neg.size() <= 1 && z > 0){puts("0");}
-    else{
-        for(long p = 0; p < pos.size(); p++){printf("%ld ", pos[p]);}
-        for(long p = neg.size() % 2; p < neg.size(); p++){printf("%ld ", neg[p]);}
+
+    if (n == 1 && neg.size() == 1) {
+        printf("%ld\n", neg[0]);
+    }
+    else if (pos.empty() && neg.size() <= 1 && z > 0) {
+        puts("0");
+    }
+    else {
+        for (long p = 0; p < (long)pos.size(); p++) {
+            printf("%ld ", pos[p]);
+        }
+        for (long p = neg.size() % 2; p < (long)neg.size(); p++) {
+            printf("%ld ", neg[p]);
+        }
         puts("");
     }
 
