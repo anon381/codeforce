97d12e91c0f1423d3ac2210ce18ab0388308a2fc
Yabets Maregn <yabetsanon@gmail.com>
2025-08-26 18:23:22 +0900

    Update 48C-TheRace.cpp

--- 48C-TheRace.cpp
+++ 48C-TheRace.cpp
@@ -1,23 +1,36 @@
-#include <cstdio>
-#include <set>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
     const long B = 1e6 + 7;
 
-    long n; scanf("%ld", &n);
-    std::set<long> s;
+    long n; 
+    scanf("%ld", &n);
+
+    set<long> s;
     long mx(0);
-    for(long p = 0; p < n; p++){long x; scanf("%ld", &x); s.insert(x); mx = (mx > x) ? mx : x;}
-    double lower(1.0), upper(B), ref(0);
-    for(long p = 1; p <= mx; p++){
-        double cand = (p + 1.0) / (ref + 1.0);
-        if(s.count(p)){upper = (upper < cand) ? upper : cand; ++ref;}
-        else{lower = (lower > cand) ? lower : cand;}
+    for (long p = 0; p < n; p++) {
+        long x; 
+        scanf("%ld", &x);
+        s.insert(x);
+        mx = (mx > x) ? mx : x;
     }
 
-    if((1 + n) * (upper - lower) >= 1){puts("not unique");}
-    else{
+    double lower(1.0), upper(B), ref(0);
+    for (long p = 1; p <= mx; p++) {
+        double cand = (p + 1.0) / (ref + 1.0);
+        if (s.count(p)) {
+            upper = (upper < cand) ? upper : cand;
+            ++ref;
+        } else {
+            lower = (lower > cand) ? lower : cand;
+        }
+    }
+
+    if ((1 + n) * (upper - lower) >= 1) {
+        puts("not unique");
+    } else {
         puts("unique");
         long next = (1 + ref) * lower;
         printf("%ld\n", next);
