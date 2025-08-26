00718b43c888d4fd74f64c77ddc29dfcfa91af82
Yabets Maregn <yabetsanon@gmail.com>
2025-08-26 18:22:21 +0900

    Update 49A-Sleuth.cpp

--- 49A-Sleuth.cpp
+++ 49A-Sleuth.cpp
@@ -1,15 +1,20 @@
-#include <cstdio>
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
+    string question;
+    getline(cin, question);
 
-    std::string question; getline(std::cin, question);
+    for (int k = question.size() - 1; k >= 0; k--) {
+        char current = tolower(question[k]);
+        if (!(current >= 'a' && current <= 'z')) continue;
 
-    for(int k = question.size(); k >= 0; k--){
-        char current = std::tolower(question[k]);
-        if(!(current >= 'a' && current <= 'z')){continue;}
-        if(current == 'a' || current == 'e' || current == 'i' || current == 'o' || current == 'u' || current == 'y'){puts("YES");}
-        else{puts("NO");}
+        if (current == 'a' || current == 'e' || current == 'i' ||
+            current == 'o' || current == 'u' || current == 'y') {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
+        }
         break;
     }
     return 0;
