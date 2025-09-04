34a7d614e351b24f50a2bc412422cde5c1b85ede
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:44:08 +0900

    Update 102B-SumOfDigits.cpp

--- 100</102B-SumOfDigits.cpp
+++ 100</102B-SumOfDigits.cpp
@@ -1,20 +1,33 @@
 #include <cstdio>
 #include <iostream>
+#include <string>
 
-long sumOfDigits(long x){
-    long output(0);
-    while(x > 0){output += x%10; x /= 10;}
+using namespace std;
+
+long sumOfDigits(long x) {
+    long output = 0;
+    while (x > 0) {
+        output += x % 10;
+        x /= 10;
+    }
     return output;
 }
 
-int main(){
+int main() {
+    string input;
+    getline(cin, input);
 
-    std::string input("");getline(std::cin, input);
-    if(input.length() == 1){puts("0");}
-    else{
-        long current(0), times(1);
-        for(long k = 0; k < input.length(); k++){current += input[k] - '0';}
-        while(current > 9){current = sumOfDigits(current); ++times;}
+    if (input.length() == 1) {
+        puts("0");
+    } else {
+        long current = 0, times = 1;
+        for (long k = 0; k < input.length(); k++) {
+            current += input[k] - '0';
+        }
+        while (current > 9) {
+            current = sumOfDigits(current);
+            ++times;
+        }
         printf("%ld\n", times);
     }
     return 0;
