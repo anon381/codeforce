0897d2c85441cd65d568fe580f79b4a0eceb9711
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:45:20 +0900

    Refactor DFS and main function for clarity

--- 100</103B-Cthulhu.cpp
+++ 100</103B-Cthulhu.cpp
@@ -1,31 +1,47 @@
 #include <cstdio>
 #include <vector>
+#include <iostream>
 
-std::vector<std::vector<int> > edges;
-std::vector<bool> visited;
-int totalVisited(0);
+using namespace std;
 
-void dfs(int node){
-    if(visited[node]){return;}
-    visited[node] = 1; ++totalVisited;
-    for(int p = 0; p < edges[node].size(); p++){dfs(edges[node][p]);}
+vector<vector<int>> edges;
+vector<bool> visited;
+int totalVisited = 0;
+
+void dfs(int node) {
+    if (visited[node]) return;
+    visited[node] = true;
+    ++totalVisited;
+    for (int neighbor : edges[node]) {
+        dfs(neighbor);
+    }
 }
 
+int main() {
+    int n, m; 
+    scanf("%d %d", &n, &m);
+    if (n != m) {
+        puts("NO");
+        return 0;
+    }
 
-int main(){
+    edges.resize(n + 1);
+    visited.resize(n + 1, false);
 
-    int n, m; scanf("%d %d\n", &n, &m);
-    if(n != m){puts("NO"); return 0;}
-
-    edges.resize(n + 1); visited.resize(n + 1);
-    for(int p = 0; p < m; p++){
-        int a, b; scanf("%d %d\n", &a, &b);
-        edges[a].push_back(b); edges[b].push_back(a);
+    for (int p = 0; p < m; p++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        edges[a].push_back(b);
+        edges[b].push_back(a);
     }
 
     dfs(1);
-    if(totalVisited < n){puts("NO");}
-    else{puts("FHTAGN!");}
+
+    if (totalVisited < n) {
+        puts("NO");
+    } else {
+        puts("FHTAGN!");
+    }
 
     return 0;
 }
