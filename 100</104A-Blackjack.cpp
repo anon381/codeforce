b76bb7be50e608be8b15956fd4ef7a76f8b58ae1
Yabets Maregn <yabetsanon@gmail.com>
2025-09-04 14:45:45 +0900

    Refactor input/output to use iostream

--- 100</104A-Blackjack.cpp
+++ 100</104A-Blackjack.cpp
@@ -1,14 +1,22 @@
-#include <cstdio>
+#include <iostream>
 
-int main(){
-    int n; scanf("%d", &n);
-    
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+
     const int queenOfSpades = 10;
     const int upperBound = 25;
     int ways[upperBound + 1] = {0};
-    for(int k = queenOfSpades + 1; k <= queenOfSpades + 11; k++){ways[k] = 4;}
+
+    for (int k = queenOfSpades + 1; k <= queenOfSpades + 11; k++) {
+        ways[k] = 4;
+    }
+
     ways[2 * queenOfSpades] = 15;
-    printf("%d\n", ways[n]);
+
+    cout << ways[n] << endl;
 
     return 0;
 }
