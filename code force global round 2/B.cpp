1ccb18b1c31e4a64c8a88a584ae404584b8c9d3f
Yabets Maregn <yabetsanon@gmail.com>
2025-09-22 03:00:18 +0900

    Refactor B.cpp to use standard namespace

--- code force global round 2/B.cpp
+++ code force global round 2/B.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
+using namespace std;
+
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int t; 
-    if (!(std::cin >> t)) return 0;
-
+    if(!(cin >> t)) return 0;
     while (t--) {
         int n; 
-        std::cin >> n;
+        cin >> n;
         int m = 2 * n;
-
-        std::vector<int> a(m + 1, 0);
-        std::vector<char> used(m + 1, 0);
-
+        vector<int> a(m + 1, 0);
+        vector<char> used(m + 1, 0);
         int cur = 1;
         for (int x = n; x >= 1; --x) {
             while (used[cur]) ++cur;
@@ -23,12 +21,11 @@ int main() {
             a[i] = a[j] = x;
             used[i] = used[j] = 1;
         }
-
         for (int i = 1; i <= m; ++i) {
-            if (i > 1) std::cout << ' ';
-            std::cout << a[i];
+            if (i > 1) cout << ' ';
+            cout << a[i];
         }
-        std::cout << '\n';
+        cout << '\n';
     }
     return 0;
 }
