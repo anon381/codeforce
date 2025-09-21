542fc2096a4ec099741b4e3885724b6c82f0a801
Yabets Maregn <yabetsanon@gmail.com>
2025-09-22 02:59:56 +0900

    Update B.cpp

--- code force global round 2/B.cpp
+++ code force global round 2/B.cpp
@@ -1,16 +1,19 @@
 #include <bits/stdc++.h>
-using namespace std;
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
     int t; 
-    if(!(cin >> t)) return 0;
+    if (!(std::cin >> t)) return 0;
+
     while (t--) {
         int n; 
-        cin >> n;
+        std::cin >> n;
         int m = 2 * n;
-        vector<int> a(m + 1, 0);
-        vector<char> used(m + 1, 0);
+
+        std::vector<int> a(m + 1, 0);
+        std::vector<char> used(m + 1, 0);
+
         int cur = 1;
         for (int x = n; x >= 1; --x) {
             while (used[cur]) ++cur;
@@ -20,11 +23,12 @@ int main() {
             a[i] = a[j] = x;
             used[i] = used[j] = 1;
         }
+
         for (int i = 1; i <= m; ++i) {
-            if (i > 1) cout << ' ';
-            cout << a[i];
+            if (i > 1) std::cout << ' ';
+            std::cout << a[i];
         }
-        cout << '\n';
+        std::cout << '\n';
     }
     return 0;
 }
