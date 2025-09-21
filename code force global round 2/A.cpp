8fc7c0007f7c7b8ef2be7dd8e54c9ef5d8b0ea0b
Yabets Maregn <yabetsanon@gmail.com>
2025-09-22 02:58:12 +0900

    Implement solution for problem A in Codeforces Round 2

--- code force global round 2/A.cpp
+++ code force global round 2/A.cpp
@@ -1,1 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; 
+    cin >> t;
+    while (t--) {
+        long long x, y; 
+        cin >> x >> y;
+        if (y > x) {
+            cout << 2 << '\n';
+        } else if (y >= 2 && x - y >= 2) {
+            cout << 3 << '\n';
+        } else {
+            cout << -1 << '\n';
+        }
+    }
+    return 0;
+}
 
