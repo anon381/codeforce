dde4eeeb8705bdbd5e4ec80f4ef687f32e6d4a60
Yabets Maregn <yabetsanon@gmail.com>
2025-10-10 19:59:03 +0900

    Refactor CardGame.cpp for clarity and efficiency

--- 100</106A-CardGame.cpp
+++ 100</106A-CardGame.cpp
@@ -1,28 +1,34 @@
-
-#include <cstdio>
 #include <iostream>
 #include <map>
+#include <cstdio>
 
-int main(){
+using namespace std;
 
-    char trump; scanf("%c\n", &trump);
-    std::string input; getline(std::cin, input);
-    std::string output("NO");
+int main() {
+    char trump;
+    scanf("%c\n", &trump);
 
-    std::map<char, int> value;
-    value.insert(std::pair<char, int>('6', 6));
-    value.insert(std::pair<char, int>('7', 7));
-    value.insert(std::pair<char, int>('8', 8));
-    value.insert(std::pair<char, int>('9', 9));
-    value.insert(std::pair<char, int>('T', 10));
-    value.insert(std::pair<char, int>('J', 11));
-    value.insert(std::pair<char, int>('Q', 12));
-    value.insert(std::pair<char, int>('K', 13));
-    value.insert(std::pair<char, int>('A', 14));
+    string input;
+    getline(cin, input);
+    string output = "NO";
 
-    if(input[1] == trump && input[4] != trump){output = "YES";}
-    else if(input[1] == input[4] && value[input[0]] > value[input[3]]){output = "YES";}
+    map<char, int> value;
+    value['6'] = 6;
+    value['7'] = 7;
+    value['8'] = 8;
+    value['9'] = 9;
+    value['T'] = 10;
+    value['J'] = 11;
+    value['Q'] = 12;
+    value['K'] = 13;
+    value['A'] = 14;
 
-    std::cout << output << std::endl;
+    if (input[1] == trump && input[4] != trump) {
+        output = "YES";
+    } else if (input[1] == input[4] && value[input[0]] > value[input[3]]) {
+        output = "YES";
+    }
+
+    cout << output << endl;
     return 0;
 }
