3167192e3dcd8c1f7c87efcc2d204cb8f658d155
Yabets Maregn <yabetsanon@gmail.com>
2025-10-10 20:02:44 +0900

    Refactor laptop selection logic in CardGame.cpp

--- 100</106B-CardGame.cpp
+++ 100</106B-CardGame.cpp
@@ -1,13 +1,15 @@
 #include <cstdio>
 #include <vector>
+using namespace std;
 
-int main(){
+int main() {
+    int n;
+    scanf("%d", &n);
 
-    int n(0); scanf("%d", &n);
-    std::vector<std::vector<int> > laptop;
-    for(int k = 0; k < n; k++){
-        laptop.push_back(std::vector<int>(4,0));
-        int speed(0), ram(0), hdd(0), cost(0);
+    vector<vector<int>> laptop;
+    for (int k = 0; k < n; k++) {
+        laptop.push_back(vector<int>(4, 0));
+        int speed, ram, hdd, cost;
         scanf("%d %d %d %d", &speed, &ram, &hdd, &cost);
         laptop[k][0] = speed;
         laptop[k][1] = ram;
@@ -15,22 +17,33 @@ int main(){
         laptop[k][3] = cost;
     }
 
-    bool *candidate = new bool[n]; for(int k = 0; k < n; k++){candidate[k] = 1;}
+    bool *candidate = new bool[n];
+    for (int k = 0; k < n; k++) candidate[k] = true;
 
-    for(int a = 0; a < n; a++){
-        for(int b = 0; b < n; b++){
-            if(a == b){continue;}
-            if(laptop[a][0] < laptop[b][0] && laptop[a][1] < laptop[b][1] && laptop[a][2] < laptop[b][2] && laptop[a][3] < laptop[b][3]){candidate[a] = 0; break;}
+    for (int a = 0; a < n; a++) {
+        for (int b = 0; b < n; b++) {
+            if (a == b) continue;
+            if (laptop[a][0] < laptop[b][0] &&
+                laptop[a][1] < laptop[b][1] &&
+                laptop[a][2] < laptop[b][2] &&
+                laptop[a][3] < laptop[b][3]) {
+                candidate[a] = false;
+                break;
+            }
         }
     }
 
-    int bestLaptop(-1), bestPrice(1001);
-    for(int k = 0; k < n; k++){
-        if(!candidate[k]){continue;}
-        if(laptop[k][3] < bestPrice){bestPrice = laptop[k][3]; bestLaptop = k;}
+    int bestLaptop = -1, bestPrice = 1001;
+    for (int k = 0; k < n; k++) {
+        if (!candidate[k]) continue;
+        if (laptop[k][3] < bestPrice) {
+            bestPrice = laptop[k][3];
+            bestLaptop = k;
+        }
     }
 
     printf("%d\n", bestLaptop + 1);
 
+    delete[] candidate;
     return 0;
 }
