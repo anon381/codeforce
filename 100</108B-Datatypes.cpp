a089ec7ecdd7e964a5c560fb1ae2fbc7f59367d8
Yabets Maregn <yabetsanon@gmail.com>
2025-10-11 17:37:43 +0900

    Refactor C++ code for improved readability

--- 100</108B-Datatypes.cpp
+++ 100</108B-Datatypes.cpp
@@ -1,23 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <cstdio>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+int main() {
 
-int main(){
+    long n; 
+    scanf("%ld\n", &n);
 
-    long n; scanf("%ld\n", &n);
-    std::vector<long> bits(n, 0);
-    for(int p = 0; p < n; p++){scanf("%ld", &bits[p]);}
-    sort(bits.begin(), bits.end());
-
-    std::string output = "NO";
-    for(int p = 1; p < n; p++){
-        if(bits[p - 1] != bits[p] && 2 * bits[p - 1] > bits[p]){output = "YES"; break;}
+    vector<long> bits(n, 0);
+    for (int p = 0; p < n; p++) {
+        scanf("%ld", &bits[p]);
     }
 
-    std::cout << output << std::endl;
+    sort(bits.begin(), bits.end());
+
+    string output = "NO";
+    for (int p = 1; p < n; p++) {
+        if (bits[p - 1] != bits[p] && 2 * bits[p - 1] > bits[p]) {
+            output = "YES";
+            break;
+        }
+    }
+
+    cout << output << endl;
 
     return 0;
 }
