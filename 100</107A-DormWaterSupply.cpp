972b543422fcbdcaccae51e7d721da2b247144a3
Yabets Maregn <yabetsanon@gmail.com>
2025-10-11 17:34:39 +0900

    Update 107A-DormWaterSupply.cpp

--- 100</107A-DormWaterSupply.cpp
+++ 100</107A-DormWaterSupply.cpp
@@ -1,43 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <cstdio>
-#include <vector>
-#include <map>
+struct wedge {
+    int source, target;
+    long diameter;
+};
 
-struct wedge{int source, target; long diameter;};
+int main() {
 
-int main(){
+    int n, p; 
+    scanf("%d %d\n", &n, &p);
 
-    int n, p; scanf("%d %d\n", &n, &p);
+    vector<int> degrees(n + 1, 0);
+    map<int, pair<int, long>> edges;
 
-    std::vector<int> degrees(n + 1, 0);
-    std::map<int, std::pair<int, long> > edges;
-
-    for(int s = 0; s < p; s++){
-        int from, to; long cap; scanf("%d %d %ld\n", &from, &to, &cap);
-        --degrees[from]; ++degrees[to]; 
-        edges.insert(std::pair<int, std::pair<int, long> >(from, std::pair<int, long>(to, cap)));
+    for (int s = 0; s < p; s++) {
+        int from, to; 
+        long cap; 
+        scanf("%d %d %ld\n", &from, &to, &cap);
+        --degrees[from]; 
+        ++degrees[to]; 
+        edges.insert({from, {to, cap}});
     }
 
-    std::vector<wedge> combined;
+    vector<wedge> combined;
 
-    for(int h = 1; h <= n; h++){
-        if(degrees[h] >= 0){continue;}
+    for (int h = 1; h <= n; h++) {
+        if (degrees[h] >= 0) continue;
 
         long cap = 1e6 + 1;
         int tap = h;
-        while(edges.count(tap) > 0){
-            if(edges[tap].second < cap){cap = edges[tap].second;}
+        while (edges.count(tap) > 0) {
+            if (edges[tap].second < cap) cap = edges[tap].second;
             tap = edges[tap].first;
         }
 
-        wedge temp; temp.source = h; temp.target = tap; temp.diameter = cap;
-        combined.push_back(temp);
-
+        combined.push_back({h, tap, cap});
     }
 
     printf("%lu\n", combined.size());
-    for(int p = 0; p < combined.size(); p++){printf("%d %d %ld\n", combined[p].source, combined[p].target, combined[p].diameter);}
-
+    for (int p = 0; p < combined.size(); p++) {
+        printf("%d %d %ld\n", combined[p].source, combined[p].target, combined[p].diameter);
+    }
 
     return 0;
 }
