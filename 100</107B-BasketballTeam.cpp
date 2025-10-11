fa07b4be952cc0f2a6ba2354b5668d70a91ddeb9
Yabets Maregn <yabetsanon@gmail.com>
2025-10-11 17:34:15 +0900

    Refactor BasketballTeam.cpp for clarity and efficiency

--- 100</107B-BasketballTeam.cpp
+++ 100</107B-BasketballTeam.cpp
@@ -1,18 +1,28 @@
-#include <cstdio>
-#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
 
-    long n, m, h; scanf("%ld %ld %ld", &n, &m, &h);
-    std::vector<long> a(m + 1, 0);
-    long sum(0); 
+    long n, m, h; 
+    scanf("%ld %ld %ld", &n, &m, &h);
 
-    for (long p = 1; p <= m; p++){scanf("%ld", &a[p]); sum += a[p];}
-    if (sum < n){puts("-1");}
+    vector<long> a(m + 1, 0);
+    long sum = 0; 
+
+    for (long p = 1; p <= m; p++) {
+        scanf("%ld", &a[p]); 
+        sum += a[p];
+    }
+
+    if (sum < n) {
+        puts("-1");
+    } 
     else {
         double s = 1.0;
-        for(long p = 1; p < n; p++){s = s * (sum + 1 - a[h] - p) / (sum - p);}
-        printf("%.10f",1-s);
+        for (long p = 1; p < n; p++) {
+            s = s * (sum + 1 - a[h] - p) / (sum - p);
+        }
+        printf("%.10f", 1 - s);
     }
 
     return 0;
