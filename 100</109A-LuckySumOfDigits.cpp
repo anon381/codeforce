030a7acb880bbab8445d77c8ef3d74f5217a3660
Yabets Maregn <yabetsanon@gmail.com>
2025-10-11 17:38:28 +0900

    Refactor LuckySumOfDigits.cpp for clarity and style

--- 100</109A-LuckySumOfDigits.cpp
+++ 100</109A-LuckySumOfDigits.cpp
@@ -1,26 +1,30 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
-    long n(0); scanf("%ld", &n);
+    long n = 0; 
+    scanf("%ld", &n);
 
-    long numSevens(-1), numFours(-1), minDigits(1e6);
+    long numSevens = -1, numFours = -1, minDigits = 1e6;
 
-    for(long testSevens = n / 7; testSevens >= 0; testSevens--){
-        if((n - 7 * testSevens) % 4 != 0){continue;}
+    for (long testSevens = n / 7; testSevens >= 0; testSevens--) {
+        if ((n - 7 * testSevens) % 4 != 0) continue;
         long testFours = (n - 7 * testSevens) / 4;
-        numSevens = testSevens; numFours = testFours; 
+        numSevens = testSevens; 
+        numFours = testFours; 
         minDigits = numSevens + numFours;
         break;
     }
 
-
-    if(numSevens >= 0 && numFours >= 0){
-        for(int k = 0; k < numFours; k++){printf("4");}
-        for(int k = 0; k < numSevens; k++){printf("7");}
+    if (numSevens >= 0 && numFours >= 0) {
+        for (int k = 0; k < numFours; k++) printf("4");
+        for (int k = 0; k < numSevens; k++) printf("7");
+    } 
+    else {
+        printf("-1");
     }
-    else{printf("-1");}
-    puts("");
 
+    puts("");
     return 0;
 }
