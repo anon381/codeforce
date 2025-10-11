4be54bf8fe6336ac783f0bf773b0b7e965255f95
Yabets Maregn <yabetsanon@gmail.com>
2025-10-11 17:36:28 +0900

    Refactor main function and improve formatting

--- 100</108A-PalindromicTimes.cpp
+++ 100</108A-PalindromicTimes.cpp
@@ -1,11 +1,24 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
 
-    int hours(0), minutes(0); scanf("%d:%d", &hours, &minutes);
+    int hours = 0, minutes = 0; 
+    scanf("%d:%d", &hours, &minutes);
+
     int nextMinutes = 10 * (hours % 10) + (hours / 10);
-    if(nextMinutes <= minutes || nextMinutes > 59){++hours; if(hours % 10 > 5){hours -= hours % 10; hours += 10;} hours %= 24;}
+
+    if (nextMinutes <= minutes || nextMinutes > 59) {
+        ++hours; 
+        if (hours % 10 > 5) {
+            hours -= hours % 10; 
+            hours += 10;
+        }
+        hours %= 24;
+    }
+
     nextMinutes = 10 * (hours % 10) + (hours / 10);
-    printf("%d%d:%d%d\n", hours/10,hours%10, nextMinutes/10,nextMinutes%10);
+    printf("%d%d:%d%d\n", hours / 10, hours % 10, nextMinutes / 10, nextMinutes % 10);
+
     return 0;
 }
