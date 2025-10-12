979d84c6b9f4ba8c10f8c2db385e5cc849be5654
Yabets Maregn <yabetsanon@gmail.com>
2025-10-12 20:17:05 +0900

    Refactor input/output and simplify logic

--- 100</111A-PetyaAndInequations.cpp
+++ 100</111A-PetyaAndInequations.cpp
@@ -1,22 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <cstdio>
-#include <vector>
+int main() {
+    long long n, x, y;
+    cin >> n >> x >> y;
 
-int main(){
-
-    long long n, x, y; scanf("%lld %lld %lld", &n, &x, &y);
-
-    bool possible = 1;
-    std::vector<long long> numbers(n, 1);
+    bool possible = true;
+    vector<long long> numbers(n, 1);
     numbers[n - 1] = y - (n - 1);
-    if(numbers[n - 1] <= 0){possible = 0;}
+    if (numbers[n - 1] <= 0) possible = false;
 
     long long sumOfSquares = 0;
-    for(int p = 0; p < n; p++){sumOfSquares += numbers[p] * numbers[p];}
-    if(sumOfSquares < x){possible = 0;}
+    for (int p = 0; p < n; p++) sumOfSquares += numbers[p] * numbers[p];
+    if (sumOfSquares < x) possible = false;
 
-    if(possible){for(int p = 0; p < n; p++){printf("%lld ", numbers[p]);}; puts("");}
-    else{puts("-1");}
+    if (possible) {
+        for (int p = 0; p < n; p++) cout << numbers[p] << " ";
+        cout << "\n";
+    } else {
+        cout << "-1\n";
+    }
 
     return 0;
 }
