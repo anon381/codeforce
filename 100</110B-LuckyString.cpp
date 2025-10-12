0427f31772ebac55083eede3619dc026584e0a5d
Yabets Maregn <yabetsanon@gmail.com>
2025-10-12 20:06:30 +0900

    Refactor main function to use cin and cout

--- 100</110B-LuckyString.cpp
+++ 100</110B-LuckyString.cpp
@@ -1,14 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <cstdio>
-
-int main(){
-
-    long n(0); scanf("%ld", &n);
-    for(int k = 0; k < n ; k++){
-        if(k % 4 == 0){printf("a");}
-        else if(k % 4 == 1){printf("b");}
-        else if(k % 4 == 2){printf("c");}
-        else if(k % 4 == 3){printf("d");}
+int main() {
+    long n = 0;
+    cin >> n;
+    for (int k = 0; k < n; k++) {
+        if (k % 4 == 0) cout << "a";
+        else if (k % 4 == 1) cout << "b";
+        else if (k % 4 == 2) cout << "c";
+        else cout << "d";
     }
-    printf("\n");
+    cout << "\n";
+    return 0;
 }
