77f829f16431bbc9ee0adeecf633b7588e1a50a1
Yabets Maregn <yabetsanon@gmail.com>
2025-10-12 20:04:17 +0900

    Refactor NearlyLuckyNumber.cpp for clarity

--- 100</110A-NearlyLuckyNumber.cpp
+++ 100</110A-NearlyLuckyNumber.cpp
@@ -1,10 +1,15 @@
-#include <iostream>
-using namespace std;
+#include <bits/stdc++.h>
+using namespace sts;
 
-int main(){
-    string number; getline(cin, number);
+int main() {
+    string number; 
+    getline(cin, number);
     int output = 0;
-    for(int k = 0; k < number.size(); ++k){if(number[k] =='4' || number[k] =='7'){++output;}}
+    for (int k = 0; k < number.size(); ++k) {
+        if (number[k] == '4' || number[k] == '7') {
+            ++output;
+        }
+    }
     cout << ((output == 4 || output == 7) ? "YES" : "NO") << endl;
     return 0;
 }
