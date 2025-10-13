f40b939d2dabe1241feac706583179a93f90cd0a
Yabets Maregn <yabetsanon@gmail.com>
2025-10-13 17:02:07 +0900

    Refactor 115A-Party.cpp to use C++ features

--- 100</115A-Party.cpp
+++ 100</115A-Party.cpp
@@ -1,21 +1,23 @@
-#include <cstdio>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
+int main() {
+    int n;
+    cin >> n;
+    vector<int> sup(n + 1);
 
-    int n(0); scanf("%d", &n);
-    int *sup = new int[n + 1];
-
-    for(int k = 1; k <= n; k++){scanf("%d", sup + k);}
+    for (int k = 1; k <= n; k++) cin >> sup[k];
 
     int total = 0;
-    for(int k = 1; k <= n; k++){
-        int temp(1), boss(sup[k]);
-        while(boss != -1){boss = sup[boss]; ++temp;}
-        if(temp > total){total = temp;}
+    for (int k = 1; k <= n; k++) {
+        int temp = 1, boss = sup[k];
+        while (boss != -1) {
+            boss = sup[boss];
+            ++temp;
+        }
+        total = max(total, temp);
     }
 
-    printf("%d\n", total);
-
-    delete[] sup;
+    cout << total << "\n";
     return 0;
 }
