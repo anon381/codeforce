07ab6f6870df4f2ecf60315f9be0b7801cef948c
Yabets Maregn <yabetsanon@gmail.com>
2025-10-14 00:53:22 +0900

    Refactor Little Pigs and Wolves solution

--- 100</116B-LittlePigsAndWolves.cpp
+++ 100</116B-LittlePigsAndWolves.cpp
@@ -1,24 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <cstdio>
-#include <iostream>
-#include <vector>
+int main() {
+    int n, m;
+    scanf("%d %d\n", &n, &m);
 
-int main(){
+    vector<string> array;
+    string line;
+    for (int k = 0; k < n; k++) {
+        getline(cin, line);
+        array.push_back(line);
+    }
 
-    int n, m; scanf("%d %d\n", &n, &m);
-    std::vector<std::string> array; std::string line;
-    for(int k = 0; k < n; k++){getline(std::cin, line); array.push_back(line);}
-
-    int eligiblePigs(0), eligibleWolves(0);
+    int eligiblePigs = 0, eligibleWolves = 0;
     char currentCell;
-    for(int row = 0; row < n; row++){
-        for(int col = 0; col < m; col++){
+
+    for (int row = 0; row < n; row++) {
+        for (int col = 0; col < m; col++) {
             currentCell = array[row][col];
-            if(currentCell == '.'){continue;}
-            if(currentCell == 'P'){if((row > 0 &&  array[row - 1][col] == 'W') || (row < n-1 &&  array[row + 1][col] == 'W') || (col > 0 &&  array[row][col - 1] == 'W') || (col < m - 1  &&  array[row][col + 1] == 'W')){++eligiblePigs;}}
-            if(currentCell == 'W'){if((row > 0 &&  array[row - 1][col] == 'P') || (row < n-1 &&  array[row + 1][col] == 'P') || (col > 0 &&  array[row][col - 1] == 'P') || (col < m - 1  &&  array[row][col + 1] == 'P')){++eligibleWolves;}}
+            if (currentCell == '.') continue;
+
+            if (currentCell == 'P') {
+                if ((row > 0 && array[row - 1][col] == 'W') ||
+                    (row < n - 1 && array[row + 1][col] == 'W') ||
+                    (col > 0 && array[row][col - 1] == 'W') ||
+                    (col < m - 1 && array[row][col + 1] == 'W')) {
+                    ++eligiblePigs;
+                }
+            }
+
+            if (currentCell == 'W') {
+                if ((row > 0 && array[row - 1][col] == 'P') ||
+                    (row < n - 1 && array[row + 1][col] == 'P') ||
+                    (col > 0 && array[row][col - 1] == 'P') ||
+                    (col < m - 1 && array[row][col + 1] == 'P')) {
+                    ++eligibleWolves;
+                }
+            }
         }
     }
-    std::cout << std::min(eligiblePigs, eligibleWolves) << std::endl;
+
+    cout << min(eligiblePigs, eligibleWolves) << endl;
     return 0;
 }
