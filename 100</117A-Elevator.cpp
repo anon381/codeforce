3aca28e8e661ebb84eb928e3b98c26f06c580ba4
Yabets Maregn <yabetsanon@gmail.com>
2025-10-14 00:54:35 +0900

    Update 117A-Elevator.cpp

--- 100</117A-Elevator.cpp
+++ 100</117A-Elevator.cpp
@@ -1,23 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
 
-#include <cstdio>
+int main() {
+    long n = 0, m = 0;
+    scanf("%ld %ld", &n, &m);
 
-int main(){
-
-    long n(0), m(0); scanf("%ld %ld", &n, &m);
-    while(n--){
-        long s(0), f(0), t(0), output(0);
+    while (n--) {
+        long s = 0, f = 0, t = 0, output = 0;
         scanf("%ld %ld %ld", &s, &f, &t);
-        if(s == f){output = t;}
-        else{
-            long T = 2 * (m - 1); //Period of a round trip
-            --s; --f;   //For modulo arithmetic to work
-            output = (t/T) * T;  
-            if(s < f && (output + s) < t){output += T;}
-            else if(s > f && (output + T - s) < t){output += T;}
+
+        if (s == f) {
+            output = t;
+        } else {
+            long T = 2 * (m - 1); // Period of a round trip
+            --s; --f; // Adjust for modulo arithmetic
+            output = (t / T) * T;
+
+            if (s < f && (output + s) < t) {
+                output += T;
+            } else if (s > f && (output + T - s) < t) {
+                output += T;
+            }
+
             output += (s < f) ? f : (T - f);
         }
+
         printf("%ld\n", output);
     }
+
     return 0;
 }
-
